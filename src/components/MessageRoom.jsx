2efc11aef949e838a9c6d3f753a13866c83c70b4
lokusuke <suzloku63@gmail.com>
2026-06-27 19:31:44 +0900

    自分のメッセージを逆並びにする書き方を修正

--- src/components/MessageRoom.jsx
+++ src/components/MessageRoom.jsx
@@ -8,22 +8,17 @@ export const MessageRoom = () => {
   // クリックされているメッセージ部屋IDからメッセージ一覧を取得する
   const activeMessages = useAtomValue(getMessagesAtom);
 
-  // console.log(activeMessageRoomId);
-  // console.log(activeMessages);
-
   return (
-    <ul className="list-none mx-10 my-10 flex-1">
-      {activeMessages.map((message) =>
-        message.sender.name === loginUser.name ? (
+    <ul className="list-none mx-10 my-10 flex-1 p-5">
+      {activeMessages.map((message) => {
+        // （メッセージの送信者が）自分であるかどうか
+        const isMe = message.sender.name === loginUser.name;
+
+        return (
           <li key={message.id}>
-            <div className="flex gap-5 p-5 items-center text-left">
-              <div className="flex flex-col border border-green-300 bg-green-300 p-2 rounded-2xl">
-                <div className="flex justify-between">
-                  <span>{message.sender.name}</span>
-                  <span className="text-gray-500">★ここに時刻</span>
-                </div>
-                <span>{message.content}</span>
-              </div>
+            <div
+              className={`flex gap-5 p-5 items-center text-left ${isMe ? "flex-row-reverse" : "flex-row"}`}
+            >
               <div>
                 <img
                   src={message.sender.avatar}
@@ -31,19 +26,9 @@ export const MessageRoom = () => {
                   className="rounded-full h-15 w-15"
                 />
               </div>
-            </div>
-          </li>
-        ) : (
-          <li key={message.id}>
-            <div className="flex gap-5 p-5 items-center text-left">
-              <div>
-                <img
-                  src={message.sender.avatar}
-                  alt={`アイコン画像 ${message.sender}`}
-                  className="rounded-full h-15 w-15"
-                />
-              </div>
-              <div className="flex flex-col border border-green-300 bg-green-300 p-2 rounded-2xl">
+              <div
+                className={`flex flex-col border rounded-2xl p-2 ${isMe ? "border-pink-200 bg-pink-200" : "border-green-200 bg-green-200"}`}
+              >
                 <div className="flex justify-between">
                   <span>{message.sender.name}</span>
                   <span className="text-gray-500">★ここに時刻</span>
@@ -52,8 +37,8 @@ export const MessageRoom = () => {
               </div>
             </div>
           </li>
-        ),
-      )}
+        );
+      })}
     </ul>
   );
 };
