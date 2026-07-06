427318b0e824aa5eb9790515630602b756925b13
lokusuke <suzloku63@gmail.com>
2026-07-06 20:32:57 +0900

    共通部品にprops (className)を追加

--- src/components/utils/Avatar.jsx
+++ src/components/utils/Avatar.jsx
@@ -1,4 +1,5 @@
-export const Avatar = ({ src, alt, size = "md" }) => {
+export const Avatar = ({ src, alt, size = "md", className = "" }) => {
+  // 画像サイズのキーバリューを用意（デフォは"md"）
   const avatarSize = {
     sm: "h-10 w-10",
     md: "h-15 w-15",
@@ -6,7 +7,11 @@ export const Avatar = ({ src, alt, size = "md" }) => {
 
   return (
     <div>
-      <img src={src} alt={alt} className={`rounded-full ${avatarSize}`} />
+      <img
+        src={src}
+        alt={alt}
+        className={`rounded-full ${avatarSize} ${className}`}
+      />
     </div>
   );
 };
